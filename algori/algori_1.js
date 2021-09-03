1f3142ba2242350e30ac33de871eb3f53edfaec5
Kim SungJin <gotninja0102@gmail.com>
2021-09-04 00:37:03 +0900

    greedy

---
+++ algori/algori_1.js
@@ -0,0 +1,22 @@
+// greedy
+let activity = [[1,1,3], [2,2,5], [3,4,7], [4,1,8], [5,5,9], [6,8,10], [7,9,11], [8,11,14], [9,13,16]];
+
+function activitySelection(act) {
+    let result = []
+    let sorted = act.sort((prev, cur) => {
+        return prev[2] - cur[2]
+    }) // using last array index to sorting activity.
+
+    let last = 0
+    sorted.forEach(el => {
+        if(last < el[1]) {
+            last = el[2]
+            result.push(el)
+        }
+    })
+    return result.map(el => {
+        return el[0]
+    })
+}
+
+activitySelection(activity)
