6dd531f236e93be69954c21030f714edde916449
Kim SungJin <gotninja0102@gmail.com>
2021-09-13 22:09:52 +0900

    algori

---
+++ algori/algo_review.js
@@ -0,0 +1,9 @@
+function getValueOfNthElement(arr, num) {
+    if(arr.length === 0) {
+      return 'no name'
+    } else if(arr.length-1 < num) {
+      return arr[arr.length-1].name
+    } else {
+      return arr[num].name
+    }
+  }
